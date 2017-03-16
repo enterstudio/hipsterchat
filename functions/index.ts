55e8d0c61a412cdc8020625995f478564dbdbeda
Lauren Long <longlauren@google.com>
2017-03-15 18:28:03 -0700

    change to typescript

--- functions/index.ts
+++ functions/index.ts
@@ -1,15 +1,15 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-const request = require("request-promise");
+import * as functions from 'firebase-functions';
 let translate = require('@google-cloud/translate')();
+import * as admin from 'firebase-admin';
+import * as request from 'request-promise';
 
 admin.initializeApp(functions.config().firebase);
 
-exports.hello = functions.https.onRequest((req, res) => {
+export let hello = functions.https.onRequest((req, res) => {
     res.send('hello, world!');
 });
 
-exports.translateMessages  = functions.database.ref('messages/{id}/msg').onWrite(event => {
+export let translateMessages  = functions.database.ref('messages/{id}/msg').onWrite(event => {
     let snap = event.data;
     translate.getLanguages().then(([langs]) => {
         let work = langs.map(lang => {
@@ -21,7 +21,7 @@ exports.translateMessages  = functions.database.ref('messages/{id}/msg').onWrite
     })
 });
 
-exports.greetNewUsers = functions.auth.user().onCreate(event => {
+export let greetNewUsers = functions.auth.user().onCreate(event => {
     let user = event.data;
     return admin.database().ref('messages/general').push({
         text: `Welcome ${user.displayName}!`,
@@ -31,7 +31,7 @@ exports.greetNewUsers = functions.auth.user().onCreate(event => {
     })
 });
 
-exports.iouMuffin = functions.analytics.event('feedback_sent').onLog(event => {
+export let iouMuffin = functions.analytics.event('feedback_sent').onLog(async event => {
   const userId = event.data.user.userId;
   return admin.database().ref('muffinClub').transaction(value => {
     // Only have 12 muffins to give away:
@@ -47,38 +47,37 @@ exports.iouMuffin = functions.analytics.event('feedback_sent').onLog(event => {
   });
 });
 
-exports.manageRoomTopics = functions.database.ref('rooms/{room}/members/{uid}').onWrite(event => {
+export let manageRoomTopics = functions.database.ref('rooms/{room}/members/{uid}').onWrite(async event => {
   let action = event.data.exists() ? 'batchAdd' : 'batchRemove';
 
-  return admin.database().ref(`pushIds/${event.params.uid}`).once('value').then(tokenSnapshot => {
-    let pushTokens = Object.keys(tokensSnapshot.val());
-    if (pushTokens.length === 0) {
-        console.log(`Cannot subscribe user ${event.params.uid} to room ${event.params.room}. They have no tokens`)
-        return null;
+  let tokensSnapshot = await admin.database().ref(`pushIds/${event.params.uid}`).once('value');
+  let pushTokens = Object.keys(tokensSnapshot.val());
+  if (pushTokens.length === 0) {
+    console.log(`Cannot subscribe user ${event.params.uid} to room ${event.params.room}. They have no tokens`)
+    return null;
+  }
+
+  console.log(`Adding ${pushTokens.length} tokens for user ${event.params.uid} to room ${event.params.room}`);
+  //let oauthToken = await config.credential.getAccessToken();
+  let options = {
+    url: `https://iid.googleapis.com/iid/v1:${action}`,
+    method: 'POST',
+    headers: {
+      Authorization: `key=${functions.config().fcm.server_key}`,//'Bearer ' + oauthToken.access_token,
+    },
+    json: true,
+    body: {
+      to: `/topics/room-${event.params.room}`,
+      registration_tokens: pushTokens
     }
+  };
 
-    console.log(`Adding ${pushTokens.length} tokens for user ${event.params.uid} to room ${event.params.room}`);
-    //let oauthToken = await config.credential.getAccessToken();
-    let options = {
-        url: `https://iid.googleapis.com/iid/v1:${action}`,
-        method: 'POST',
-        headers: {
-        Authorization: `key=${functions.config().fcm.server_key}`,//'Bearer ' + oauthToken.access_token,
-        },
-        json: true,
-        body: {
-        to: `/topics/room-${event.params.room}`,
-        registration_tokens: pushTokens
-        }
-    };
-
-    return request(options);
-  });
+  await request(options);
   // TODO: A fuller implementation could remove tokens that were no longer valid.
   // Response is a results object with an array of errors or {} for no error.
 });
 
-exports.sendMentions = functions.database.ref('rooms/{room}/messages/{id}/msg').onWrite(event => {
+export let sendMentions = functions.database.ref('rooms/{room}/messages/{id}/msg').onWrite(event => {
   if (!event.data.val().match(/@here/)) {
     return "No @-mention";
   }
@@ -94,4 +93,3 @@ exports.sendMentions = functions.database.ref('rooms/{room}/messages/{id}/msg').
 
   return admin.messaging().sendToTopic(topic, payload);
 });
-
